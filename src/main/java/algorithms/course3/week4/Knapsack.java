7d4e086e44f0419e3ab716958c342f49228c0833
chekalin <stanislav.chekalin@gmail.com>
2019-11-15 10:55:03 +0000

    Course 3 Week 4: knapsack 2 (larger data set)
    refactored tests to be parametrized so that I could reuse them on alternative solution

--- src/main/java/algorithms/course3/week4/Knapsack.java
+++ src/main/java/algorithms/course3/week4/Knapsack.java
@@ -1,7 +1,7 @@
 package algorithms.course3.week4;
 
+import java.util.Arrays;
 import java.util.List;
-import java.util.stream.IntStream;
 
 class Knapsack {
 
@@ -9,26 +9,58 @@ class Knapsack {
         if (elements.isEmpty()) return 0;
 
         long[][] solutions = new long[elements.size()][capacity + 1];
+
         Element firstElement = elements.get(0);
-        IntStream.range(0, capacity + 1)
-                .forEach(c -> solutions[0][c] = firstElement.weight > c ? 0 : firstElement.value);
+        for (int c = 0; c < capacity + 1; c++) {
+            solutions[0][c] = firstElement.getWeight() > c ? 0 : firstElement.getValue();
+        }
 
         for (int i = 1; i < elements.size(); i++) {
-            for (int c = 1; c <= capacity; c++) {
-                Element currentElement = elements.get(i);
-                int currentWeight = currentElement.weight;
-                int currentValue = currentElement.value;
+            Element currentElement = elements.get(i);
+            int currentWeight = currentElement.getWeight();
 
+            for (int c = 1; c <= capacity; c++) {
                 long solutionWithoutCurrent = solutions[i - 1][c];
                 if (currentWeight > c) {
                     solutions[i][c] = solutionWithoutCurrent;
                 } else {
-                    long solutionWithCurrent = solutions[i - 1][c - currentWeight] + currentValue;
+                    long solutionWithCurrent = solutions[i - 1][c - currentWeight] + currentElement.getValue();
                     solutions[i][c] = Math.max(solutionWithoutCurrent, solutionWithCurrent);
                 }
             }
         }
         return solutions[elements.size() - 1][capacity];
+    }
+
+    /**
+     * Optimizes previous solution by using only two columns in solutions array. This approach will not allow to
+     * reconstruct what is the actual set of elements in optimal solution, but allows to handle larger data sets
+     */
+    static long solveBig(List<Element> elements, int capacity) {
+        if (elements.isEmpty()) return 0;
+
+        long[][] solutions = new long[2][capacity + 1];
+
+        Element firstElement = elements.get(0);
+        for (int c = 0; c < capacity + 1; c++) {
+            solutions[0][c] = firstElement.getWeight() > c ? 0 : firstElement.getValue();
+        }
+
+        for (int i = 1; i < elements.size(); i++) {
+            Element currentElement = elements.get(i);
+            int currentWeight = currentElement.getWeight();
+            for (int c = 1; c <= capacity; c++) {
+                long solutionWithoutCurrent = solutions[0][c];
+                if (currentWeight > c) {
+                    solutions[1][c] = solutionWithoutCurrent;
+                } else {
+                    long solutionWithCurrent = solutions[0][c - currentWeight] + currentElement.getValue();
+                    solutions[1][c] = Math.max(solutionWithoutCurrent, solutionWithCurrent);
+                }
+            }
+            solutions[0] = Arrays.copyOf(solutions[1], capacity + 1);
+        }
+        return solutions[0][capacity];
     }
 
     static class Element {
@@ -38,6 +70,14 @@ class Knapsack {
         Element(int value, int weight) {
             this.weight = weight;
             this.value = value;
+        }
+
+        int getWeight() {
+            return weight;
+        }
+
+        int getValue() {
+            return value;
         }
 
         @Override
--- src/test/java/algorithms/course3/week4/KnapsackTest.java
+++ src/test/java/algorithms/course3/week4/KnapsackTest.java
@@ -1,110 +1,123 @@
 package algorithms.course3.week4;
 
-import algorithms.course3.week4.Knapsack.Element;
-import org.assertj.core.util.Lists;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.function.BiFunction;
+import java.util.stream.Stream;
 
+import static algorithms.course3.week4.Knapsack.*;
+import static algorithms.course3.week4.KnapsackTest.TestCase.testCase;
 import static algorithms.util.AssignmentInputReader.getScanner;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class KnapsackTest {
 
-    @Test
-    void returnsZeroWhenThereAreNoItems() {
-        long solution = Knapsack.solve(Lists.emptyList(), 0);
-        assertThat(solution).isEqualTo(0);
+    private static Stream<TestCase> provideTestCases() {
+        return List.of(
+                testCase("Empty elements", List.of(), 0, 0),
+                testCase("Zero capacity", List.of(new Element(1, 1)), 0, 0),
+                testCase("Single element fits", List.of(new Element(2, 9)), 9, 2),
+                testCase("One out of two elements fit", List.of(
+                        new Element(2, 9),
+                        new Element(2, 10)
+                ), 9, 2),
+                testCase("Both elements fit", List.of(
+                        new Element(8, 5),
+                        new Element(7, 4)
+                ), 10, 15),
+                testCase("Last two elements compose better solution", List.of(
+                        new Element(8, 5), // should skip this element
+                        new Element(7, 4),
+                        new Element(12, 4)
+                ), 8, 19),
+                testCase("Example from book", List.of(
+                        new Element(3, 4),
+                        new Element(2, 3),
+                        new Element(4, 2),
+                        new Element(4, 3)
+                ), 6, 8)
+        ).stream();
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideTestCases")
+    void parametrizedBigKnapsackTest(TestCase testCase) {
+        assertThat(solveBig(testCase.elements, testCase.capacity)).isEqualTo(testCase.expectedResult);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideTestCases")
+    void parametrizedKnapsackTest(TestCase testCase) {
+        assertThat(solve(testCase.elements, testCase.capacity)).isEqualTo(testCase.expectedResult);
     }
 
     @Test
-    void returnsWhenCapacityIsZero() {
-        long solution = Knapsack.solve(List.of(new Element(1, 1)), 0);
-        assertThat(solution).isEqualTo(0);
-    }
-
-    @Test
-    void returnsSingleValueWhenItFitsCapacity() {
-        List<Element> elements = List.of(new Element(2, 9));
-        int capacity = 9;
-        long solution = Knapsack.solve(elements, capacity);
-        assertThat(solution).isEqualTo(2);
-    }
-
-    @Test
-    void doesNotAddElementsThatExceedCapacityToValue() {
-        List<Element> elements = List.of(
-                new Element(2, 9),
-                new Element(2, 10)
-        );
-        int capacity = 9;
-        long solution = Knapsack.solve(elements, capacity);
-        assertThat(solution).isEqualTo(2);
-    }
-
-    @Test
-    void addsElementsThatFit() {
-        List<Element> elements = List.of(
-                new Element(8, 5),
-                new Element(7, 4)
-        );
-        int capacity = 10;
-        long solution = Knapsack.solve(elements, capacity);
-        assertThat(solution).isEqualTo(15);
-    }
-
-    @Test
-    void picksTheBestCombinationOfElements() {
-        List<Element> elements = List.of(
-                new Element(8, 5), // should not pick this one
-                new Element(7, 4),
-                new Element(12, 4)
-        );
-        int capacity = 8;
-        long solution = Knapsack.solve(elements, capacity);
-        assertThat(solution).isEqualTo(19);
-    }
-
-    @Test
-    void moreComplexScenario() {
-        List<Element> elements = List.of(
-                new Element(3, 4),
-                new Element(2, 3),
-                new Element(4, 2),
-                new Element(4, 3)
-        );
-        int capacity = 6;
-        long solution = Knapsack.solve(elements, capacity);
-        assertThat(solution).isEqualTo(8);
-    }
-
-    @Test
-    void input_random_10_100_10() throws FileNotFoundException {
-        long result = solveFile("course3/week4/input_random_10_100_10.txt");
+    void input_random_10_100_10_with_big() throws FileNotFoundException {
+        long result = solveFile("course3/week4/input_random_10_100_10.txt", Knapsack::solveBig);
         assertThat(result).isEqualTo(147);
     }
 
     @Test
-    void assignment() throws FileNotFoundException {
-        long result = solveFile("course3/week4/problem_set_1.txt");
-        System.out.println("result = " + result);
+    void input_random_10_100_10() throws FileNotFoundException {
+        long result = solveFile("course3/week4/input_random_10_100_10.txt", Knapsack::solve);
+        assertThat(result).isEqualTo(147);
     }
 
-    private long solveFile(String filename) throws FileNotFoundException {
+    @Test
+    void assignment1() throws FileNotFoundException {
+        long result = solveFile("course3/week4/problem_set_1.txt", Knapsack::solve);
+        System.out.println("assignment 1 result = " + result);
+    }
+
+    @Test
+    @Disabled("slow")
+    void assignment2() throws FileNotFoundException {
+        long result = solveFile("course3/week4/problem_set_2.txt", Knapsack::solveBig);
+        System.out.println("assignment 2 result = " + result);
+    }
+
+    private long solveFile(String filename, BiFunction<List<Element>, Integer, Long> solver) throws FileNotFoundException {
         Scanner scanner = getScanner(filename);
         String[] meta = scanner.nextLine().split(" ");
         int capacity = Integer.parseInt(meta[0]);
         int numberOfElements = Integer.parseInt(meta[1]);
         List<Element> elements = new ArrayList<>(numberOfElements);
-        while(scanner.hasNextLine()) {
+        while (scanner.hasNextLine()) {
             String[] elementLine = scanner.nextLine().split(" ");
             int value = Integer.parseInt(elementLine[0]);
             int weight = Integer.parseInt(elementLine[1]);
             elements.add(new Element(value, weight));
         }
-        return Knapsack.solve(elements, capacity);
+        return solver.apply(elements, capacity);
+    }
+
+    static class TestCase {
+        String name;
+        List<Element> elements;
+        int capacity;
+        long expectedResult;
+
+        private TestCase(String name, List<Element> elements, int capacity, long expectedResult) {
+            this.name = name;
+            this.elements = elements;
+            this.capacity = capacity;
+            this.expectedResult = expectedResult;
+        }
+
+        static TestCase testCase(String name, List<Element> elements, int capacity, long expectedResult) {
+            return new TestCase(name, elements, capacity, expectedResult);
+        }
+
+        @Override
+        public String toString() {
+            return name;
+        }
     }
 }
